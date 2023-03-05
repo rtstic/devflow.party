8035b718abccf52edc5810e8a08fdadefbf20551
Rodney Urquhart <rodney.urquhart@webflow.com>
2023-03-05 06:20:36 -0800

    Added site info page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "screenshots.webflow.com",
+        port: '',
+        pathname: "/**",
+      }
+    ],
+  },
 }
 
 module.exports = nextConfig
